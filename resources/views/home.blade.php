276cabd6af205f62079e209e0bf7d03630055279
Ashvin <ashvinsunga@yahoo.com>
2025-04-12 00:14:53 +0800

    Added featured products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
     </section>
 
     <section id="new" class="w-100">
-        <div class="row p-0 m-0">
+        <div class="row p-0 m-0 m-auto">
             {{-- One --}}
             <div class="one col-lg-4 col-md-12 col-sm-12 p-0">
                 <img class="img-fluid" src="{{ asset('images/1.jpg') }}" alt="">
@@ -47,6 +47,73 @@
                     <button>Shop now</button>
                 </div>
             </div>
+        </div>
+    </section>
+
+    <section id="featured" class="my-5 pb-5">
+        <div class="container text-center mt-5 py-5">
+            <h3>Featured</h3>
+            <hr>
+            <p>Please check our featured products</p>
+        </div>
+
+        <div class="row mx-auto container-fluid">
+            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
+                <img src="{{ asset('images/featured1.jpg') }}" class="img-fluid mb-3">
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h5 class="p-name">Sports Bags</h5>
+                <h4 class="p-price">$100.99</h4>
+                <button class="buy-btn">Buy Now</button>
+            </div>
+
+            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
+                <img src="{{ asset('images/featured2.jpg') }}" class="img-fluid mb-3">
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h5 class="p-name">Sports Bags</h5>
+                <h4 class="p-price">$100.99</h4>
+                <button class="buy-btn">Buy Now</button>
+            </div>
+
+            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
+                <img src="{{ asset('images/featured3.jpg') }}" class="img-fluid mb-3">
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h5 class="p-name">Sports Bags</h5>
+                <h4 class="p-price">$100.99</h4>
+                <button class="buy-btn">Buy Now</button>
+            </div>
+
+            <div class="product text-center col-lg-3 col-md-4 col-sm-12">
+                <img src="{{ asset('images/featured4.jpg') }}" class="img-fluid mb-3">
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <h5 class="p-name">Sports Bags</h5>
+                <h4 class="p-price">$100.99</h4>
+                <button class="buy-btn">Buy Now</button>
+            </div>
+
         </div>
     </section>
 @endsection
